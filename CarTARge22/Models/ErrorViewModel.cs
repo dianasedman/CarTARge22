8b6d3214075d8dcf5ddceee72c891a1b102a8367
Diana Sedman <dianasedman@gmail.com>
2023-12-23 12:53:18 +0200

    Add project files.

---
+++ CarTARge22/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarTARge22.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
