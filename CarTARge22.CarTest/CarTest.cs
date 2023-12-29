f1e0724c994559d196467e7d1ec843461fca12e7
Diana Sedman <dianasedman@gmail.com>
2023-12-29 19:01:50 +0200

    CarTest created and finished

---
+++ CarTARge22.CarTest/CarTest.cs
@@ -0,0 +1,92 @@
+﻿using CarTARge22.Core.Dto;
+using CarTARge22.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTARge22.CarTest
+{
+    public class CarTest : TestBase
+    {
+
+        [Fact]
+        public async Task ShouldNot_DeleteByIdCar_WhenDidNotDeleteCar()
+        {
+            CarDto car = MockCarData();
+
+            var carOne = await Svc<ICarsServices>().Create(car);
+            var carTwo = await Svc<ICarsServices>().Create(car);
+
+            var result = await Svc<ICarsServices>().Delete((Guid)carOne.Id);
+
+            Assert.NotEqual(result.Id, carTwo.Id);
+        }
+
+        [Fact]
+        public async Task ShouldNot_AddEmptyCar_WhenReturnResult()
+        {
+            //Arrange
+            CarDto car = new();
+
+            car.Name = "Accord";
+            car.Brand = "Honda";
+            car.Year = DateTime.Now;
+            car.Transmission = TransmissionType.Automatic;
+            car.Color = "white";
+            car.Fuel = "Regular";
+            car.TopSpeed = 200;
+            car.CreatedAt = DateTime.Now;
+            car.ModifiedAt = DateTime.Now;
+
+            //Act
+            var result = await Svc<ICarsServices>().Create(car);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task ShouldNot_GetByIdCar_WhenReturnNotEqual()
+        {
+            CarDto car = new();
+            Guid wrongId = Guid.Parse(Guid.NewGuid().ToString());
+            Guid guid = Guid.Parse("1e1426e3-6846-4105-1e44-f9e5fb9f8b25");
+
+            await Svc<ICarsServices>().DetailsAsync(guid);
+
+            Assert.NotEqual(wrongId, guid);
+        }
+
+        [Fact]
+        public async Task ShouldNot_AddEmptyCAr_WhenReturnResult()
+        {
+            CarDto car = MockCarData();
+
+            var result = await Svc<ICarsServices>().Create(car);
+
+            Assert.NotNull(result);
+        }
+
+        private CarDto MockCarData()
+        {
+            CarDto car = new()
+            {
+                Id = null,
+                Name = "E46",
+                Brand = "BMW",
+                Year = DateTime.Now.AddYears(-25),
+                Transmission = TransmissionType.Manual,
+                Color = "gray",
+                Fuel = "Petrol",
+                TopSpeed = 125,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+
+            };
+
+            return car;
+        }
+    }
+}
---
+++ CarTARge22.CarTest/TestBase.cs
@@ -0,0 +1,67 @@
+﻿using CarTARge22.ApplicationServices.Services;
+using CarTARge22.CarTest.Macros;
+using CarTARge22.CarTest.Mock;
+using CarTARge22.Core.ServiceInterface;
+using CarTARge22.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTARge22.CarTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider ServiceProvider { get; }
+        protected TestBase() { 
+        var services = new ServiceCollection();
+            SetupServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        { }
+
+        protected T Svc<T>()
+        {
+            return ServiceProvider.GetService<T>();
+        }
+
+        protected T Macro<T>() where T : IMacros
+        {
+            return ServiceProvider.GetService<T>();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<ICarsServices, CarsServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<CarTARge22Context>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e => e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                RegisterMacros(services);
+            });
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(x => macroBaseType.IsAssignableFrom(x) && !!x.IsInterface && !x.IsAbstract);
+
+            foreach (var macro in macros )
+            {
+                services.AddSingleton(macro);
+            }
+        }
+
+    }
+}
